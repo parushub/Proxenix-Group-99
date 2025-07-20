f0fc142d5524a6e8c349b23c2767dd1dd826ce14
Navneet77k <naveeraj62@gmail.com>
2025-07-20 21:40:06 +0530

    Designed login page UI

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import userRouter from "./routes/userRoutes";
 
 const app = express();
 const server = http.createServer(app)
@@ -12,7 +13,9 @@ app.use(express.json({limit: "4mb"}))
 app.use(cors());
 
 
+//routes setup
 app.use("/api/status",(req, res)=> res.send("Server is live"));
+app.use("/api/auth", userRouter);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log("Server is running on PORT:" +PORT))
